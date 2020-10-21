df7f8c71087c1764246a9fcb2b7b7d45adfc118b
Manik Jain <44458643+Manikj11@users.noreply.github.com>
2020-10-21 11:02:07 +0530

    Add files via upload

---
+++ Anagram.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+d = dict() 
+li=list()
+
+#li=['eat','ate','apt','pat','tea','now'] #given list of examples
+
+x=None
+while x!='done': #taking input from user till he enters 'done'
+    x=input()
+    if x!='done':
+        li.append(x)
+    
+for i in li:
+    temp = " ".join(sorted(list(i))) #sorting individual element of list
+    if temp not in d: 
+        d[temp] =[i]   
+    else:
+        d[temp] +=[i]
+ana=list()
+for j in d.values(): #to obtain only values 
+    ana.append(j)
+print(ana)
+
